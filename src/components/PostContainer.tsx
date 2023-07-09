47c365f2589a8a49bf5ffcda9da31f1a7b95b23a
Aandi1989 <navaliny1989@gmail.com>
2023-07-09 09:53:16 +0300

    RTK query with long polling

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -4,18 +4,16 @@ import PostItem from './PostItem';
 
 const PostContainer = () => {
     const [limit, setLimit] = useState(10)
-    const { data: posts, error, isLoading, refetch } = postAPI.useFetchAllPostsQuery(limit)
+    const { data: posts, error, isLoading, refetch } = postAPI.useFetchAllPostsQuery(limit, {
+        pollingInterval: 1000 // запрос будет отправляться с интервалом в 1 секунду
+    })
 
     useEffect(() => {
-        // setTimeout(() => {
-        //     setLimit(3)
-        // }, 2000)
     },[])
 
     return (
         <div>
             <div className='post__list'>
-                <button onClick={() => refetch()}>REFETCH</button>
                 {isLoading && <h1>Идет загрузка...</h1>}
                 {error && <h1>Произошла ошибка при загрузке</h1>}
                 {posts && posts.map(post =>
