b5d8ba5a64d8691ec5f3d6e938ec0c3d6c58d6f5
Aandi1989 <navaliny1989@gmail.com>
2023-07-09 09:46:18 +0300

    RTK query with request from two component

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,26 @@ import './App.css';
 import { useAppDispatch, useAppSelector } from './hooks/redux';
 import { userSlice } from './store/reducers/UserSlice';
 import { fetchUsers } from './store/reducers/ActionCreator';
+import PostContainer from './components/PostContainer';
+import PostContainer2 from './components/PostContainer2';
 
 function App() {
-  const dispatch = useAppDispatch()
-  const { users, isLoading, error } = useAppSelector(state => state.userReducer)
+  // const dispatch = useAppDispatch()
+  // const { users, isLoading, error } = useAppSelector(state => state.userReducer)
 
-  useEffect(()=>{
-    dispatch(fetchUsers())
-  },[])
+  // useEffect(()=>{
+  //   dispatch(fetchUsers())
+  // },[])
 
   return (
     <div className="App">
-      {isLoading && <h1>Идет загрузка...</h1>}
+      {/* {isLoading && <h1>Идет загрузка...</h1>}
       {error && <h1>{error}</h1>}
-      {JSON.stringify(users, null, 2)}
+      {JSON.stringify(users, null, 2)} */}
+      <div className='flex'>
+        <PostContainer />
+        <PostContainer2 />
+      </div>
     </div>
   );
 }
